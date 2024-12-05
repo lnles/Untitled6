7da9a1b3a63af8fe21c0c15ad7a8b571cd2c1e88
lnles <lnles@aol.com>
2024-12-05 21:05:51 +0800

    updated package

--- lib/core/extensions/extensions.dart
+++ lib/core/extensions/extensions.dart
@@ -2,3 +2,4 @@ export 'utilities.dart';
 export 'margins.dart';
 export 'paddings.dart';
 export 'numbs.dart';
+export 'list_extensions.dart';
--- lib/core/extensions/list_extensions.dart
+++ lib/core/extensions/list_extensions.dart
@@ -1,3 +1,7 @@
 extension ListToMapExtension<K, V> on List<MapEntry<K, V>> {
   Map<K, V> get asMap => Map.fromEntries(this);
 }
+
+extension IterableToMapExtension<K, V> on Iterable<MapEntry<K, V>> {
+  Map<K, V> get asMap => Map.fromEntries(this);
+}
