989d2c49858e148e09d865968d9c002e6a5189f0
yongguosuo <lnles@aol.com>
2024-12-20 09:47:21 +0800

    update something

--- lib/core/extensions/text_extensions.dart
+++ lib/core/extensions/text_extensions.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 extension TextFontWeight on TextStyle {
-  TextStyle get bold => copyWith(fontWeight: FontWeight.bold);
+  TextStyle bold() => copyWith(fontWeight: FontWeight.bold);
 
-  TextStyle get semiBold => copyWith(fontWeight: FontWeight.w600);
+  TextStyle semiBold() => copyWith(fontWeight: FontWeight.w600);
 
-  TextStyle get medium => copyWith(fontWeight: FontWeight.w500);
+  TextStyle medium() => copyWith(fontWeight: FontWeight.w500);
 
-  TextStyle get regular => copyWith(fontWeight: FontWeight.normal);
+  TextStyle regular() => copyWith(fontWeight: FontWeight.normal);
 
-  TextStyle get light => copyWith(fontWeight: FontWeight.w300);
+  TextStyle light() => copyWith(fontWeight: FontWeight.w300);
 
-  TextStyle get extraLight => copyWith(fontWeight: FontWeight.w200);
+  TextStyle extraLight() => copyWith(fontWeight: FontWeight.w200);
 
-  TextStyle get thin => copyWith(fontWeight: FontWeight.w100);
+  TextStyle thin() => copyWith(fontWeight: FontWeight.w100);
 }
